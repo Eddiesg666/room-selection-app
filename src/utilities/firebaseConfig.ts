f92cb15a9aae1e84223bdbd923f517d5479a07cb
Eddie Shangguan <eddieshangguan99@gmail.com>
2025-10-17 04:04:21 -0500

    final version

--- src/utilities/firebaseConfig.ts
+++ src/utilities/firebaseConfig.ts
@@ -2,6 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
+
+console.log('Database URL:', import.meta.env.VITE_FIREBASE_DATABASE_URL);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
